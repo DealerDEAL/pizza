8602eedfab12059489ff982ab7bc5d47a47cc6cd
DealerDEAL <112918405+DealerDEAL@users.noreply.github.com>
2023-11-21 13:03:08 +0700

    Create pizzeria.h

---
+++ pizzeria.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <time.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+
+class Pizzeria {
+    public:
+    int arr = 3;
+    string name[3];
+    unsigned int ratting[3];
+    int Pizzeria_data(){ 
+        for(int i = 0; i < arr; i++){
+            ratting[i] = i + 1;
+            cin >> name[i];
+            cout << name[i] << setw(3) << ratting[i] << "\n";
+            return 0;
+        }
+    }
+};
+    
