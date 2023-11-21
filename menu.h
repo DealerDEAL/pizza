932366ddde2aed396517cff2880c7fe196b68851
DealerDEAL <112918405+DealerDEAL@users.noreply.github.com>
2023-11-21 15:04:10 +0700

    Add files via upload

---
+++ menu.h
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <time.h>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include "Random.h"
+#include "Pizzeria.h"
+
+using namespace std;
+
+void Ph(int i){
+    Pizzeria c;
+    c.Pizzeria_data(i);
+}
+
+class menu{
+    public:
+    int id[5];
+    string pizza_name[5];
+    int prise[5];
+    int manu_data(int i){
+        for(i = 0; i < 5; i++){
+            id[i] = i;
+            prise[i] = my_rand();
+            cin << pizza_name[i];
+            
+        }
+    }
+};
