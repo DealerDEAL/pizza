9d91bd087614dcbf7103717d86cdd33300218299
DealerDEAL <112918405+DealerDEAL@users.noreply.github.com>
2023-11-22 10:50:08 +0300

    Update menu.h

--- menu.h
+++ menu.h
@@ -1,31 +1,24 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h> 
-#include <time.h>
-#include <iostream>
-#include <iomanip>
-#include <fstream>
-#include "Pizzeria (1).h"
+#include "pizzeria (1).h"
 
 using namespace std;
-
-void pd(int i){
-    Pizzeria c;
-    c.Pizzeria_data(i);
-}
 
 class menu{
     public:
     int id[5];
     string pizza_name[5];
     int prise[5];
-    int md(int i, Pizzeria c){
+    int Md(int i, Pizzeria c){
+        int h = 0;
         for(i = 0; i < 5; i++){
             id[i] = i;
-            prise[i] = my_rand();
+            prise[i] = my_rand() * 125;
             cin >> pizza_name[i];
-            cout << id[i] << setw(3) << pizza_name[i] << setw(3) << prise[i] << "\n";
-            cout << c.id[i] <<"\n";
+            cout << pizza_name[i] << setw(3) << prise[i] << setw(3) << id[i] << setw(3);
+            cout << c.id[h] << "\n";
+            h++;
+            if(h > 3){
+                h = 0;
+            }
         }
         return 0;
     }
