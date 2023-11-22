ef6061273a6755fa560a9979e1f6c5e2c8d7c0f5
DealerDEAL <112918405+DealerDEAL@users.noreply.github.com>
2023-11-22 14:21:53 +0300

    Update person_order.h

--- person_order.h
+++ person_order.h
@@ -25,3 +25,26 @@ class person_order{
         return 0;
     }
 };
+
+class person_visit{
+    public:
+    int id[10], visit_date[10];
+    int Pv(int i, Pizzeria c, person b){
+        int h = 0, l = 0;
+        for(i = 0; i < 10; i++){
+            id[i] = i + 1;
+            visit_date[i] = 0;
+            cout << id[i] << setw(3) << b.id[h] << setw(3) << c.id[l] << setw(3);
+            cout << visit_date[i] << "\n";
+            if(h > 7){
+                h--;
+            }
+            if(l > 3){
+                l--;
+            }
+            h++;
+            l++;
+        }
+        return 0;
+    }
+};
